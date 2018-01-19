3aac21ec5900ff0771ba2ea455fe4cf10121d944
ludan <luludandan8@163.com>
2018-01-19 18:02:08 +0800

    01191802

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import Required, Length
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
+from wtforms.validators import Required, Length, Email
 
 
 class EditProfileForm(FlaskForm):
     name - StringField('Reale name', validators=[length(0,64)])
     location = StringField('location', validators=[length(0,64)])
-    about_me = TextAreaField('Aboute me')
+    about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
+
+class EditProfileAdminForm(FlaskForm):
+    email = StringField('Email', validators=[Required(), Length(1,64), Email()])
+    username = StringField('Username', validators=[Required(), Length(1, 64), Regexp('^[A-Za-z0-0_.]*$', 0, 'Usernames must have only letters, numbers, dots or underscores')])
+    confirmed = BooleanField('Confirmed')
+    role = SelectField('Role', coerce=int)
+    name = StringField('Real name', validators=[Length(0,64)])
+    location = StringField('location', validators=[length(0,64)])
+    about_me = StringField('About me')
+    submit = SubmitField('Submit')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name) for role in Role.query.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self, field):
+        if field.data != self.user.email and User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registed.')
+    
+    def validate_username(self, field):
+        if field.data != self.user.username and User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already registed.')
+    
--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for
 from flask_login import login_required, current_user
 from ..models import User
 from . import main
-from .forms import EditProfileForm
+from .forms import EditProfileForm, EditProfileAdminForm
 
 
 @main.route('/')
@@ -16,7 +16,7 @@ def user(username):
         abort(404)
     return render_template('user.html', user=user)
 
-@main.route('/edit-profile', method=['GET', 'POST'])
+@main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
     form = EditProfileForm()
@@ -31,3 +31,29 @@ def edit_profile():
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', form=form)
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name - form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        user.session.add(user)
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.uername))
+    form.email.data = user.role_id
+    form.username.data = user.username
+    form.confirmed = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
